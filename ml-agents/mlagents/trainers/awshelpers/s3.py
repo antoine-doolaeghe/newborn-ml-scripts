d9103f423234a06a10f064acc3b15d9275e05526
Antoine Doolaeghe <antoine.doolaeghe@draysonhealth.com>
2019-05-23 22:47:22 +0100

    NEW-70: updated bucket name

--- ml-agents/mlagents/trainers/awshelpers/s3.py
+++ ml-agents/mlagents/trainers/awshelpers/s3.py
@@ -4,4 +4,4 @@ import boto3
 def push_model_to_s3(brain_name):
     client = boto3.client('s3')
     client.upload_file("models/ppo-0/"+brain_name+".nn",
-                       "newbornbucket999999", brain_name+".nn")
+                       "newborn-training-models", brain_name+".nn")
